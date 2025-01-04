3be405ac30b845e4a4d50c5fa2a0ab1d08a388f7
Anushka Dubey <136051483+anushkaa-dubey@users.noreply.github.com>
2025-01-05 00:01:26 +0530

    added operations

---
+++ list operations.py
@@ -0,0 +1,22 @@
+#SLICING 
+lst=[1,2,3,4,5]
+print(lst[1:4])
+#APPENDING
+lst.append(6)
+#FINDING INDEX
+print(lst.index(3))
+#SORTING
+lst.sort()
+#POPPING
+lst.pop()
+#REMOVING
+lst.remove(2)
+#INSERTING
+lst.insert(1,7)
+#COUNT OCCURRENCES
+print(lst.count(7))
+#EXTENDING
+lst.extend([8,9])
+#REVERSE
+lst.reverse()
+print(lst)
