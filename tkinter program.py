efebd26833472794bf62e43daeafe7fb361f80e4
Anushka Dubey <136051483+anushkaa-dubey@users.noreply.github.com>
2025-01-09 23:26:13 +0530

    Update tkinter program.py

--- tkinter program.py
+++ tkinter program.py
@@ -1,11 +1,11 @@
 #Used .pack() here 
-
+#Question : To print 3 boxes of different height and width
 from tkinter import *
 def create_window():
     # Create and pack the red rectangle
     red_frame = Frame(root, bg="red", width=100, height=100)
     red_frame.pack(side=TOP, pady=(20, 0))
-
+    
     # Create and pack the yellow rectangle
     yellow_frame = Frame(root, bg="yellow", width=70, height=70)
     yellow_frame.pack(side=TOP, pady=(10, 0))
